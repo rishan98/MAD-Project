1353884c78c4f59e057a813a5ef6b4ce445f89cf
rishxn98 <59877117+rishan98@users.noreply.github.com>
2021-01-15 09:53:28 +0530

    Delete lib directory

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:project_autohub/screens/welcome.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(welcome());
-}
