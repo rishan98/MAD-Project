6ca2f93e0824947e546dfa5e8c0ed390d9fbdee7
rishxn98 <59877117+rishan98@users.noreply.github.com>
2021-01-21 19:52:21 +0530

    Update spareParts.dart

--- spareParts.dart
+++ spareParts.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:project_autohub/screens/spareParts01.dart';
 
@@ -7,6 +9,89 @@ class spareParts extends StatefulWidget {
 }
 
 class _sparePartsState extends State<spareParts> {
+  Query _ref;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _ref = FirebaseDatabase.instance
+        .reference()
+        .child('spare')
+        .orderByChild('sname');
+  }
+
+  Widget _buildContactItem({Map contact}) {
+    return Container(
+      child: Column(
+        children: [
+          Column(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                padding: EdgeInsets.fromLTRB(10, 40, 10, 30),
+                decoration: BoxDecoration(
+                    color: Colors.white60,
+                    borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                child: Row(
+                  children: [
+                    Container(
+                        child: Image(
+                      image: AssetImage('lib/img/p1.PNG'),
+                    )),
+                    Column(
+                      children: [
+                        Container(
+                            padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                            child: Text(
+                              contact['sname'],
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.w500),
+                            )),
+                        Container(
+                            padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                            child: Text(
+                              contact['price'],
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w500),
+                            )),
+                        Container(
+                            child: Text(
+                          contact['availability'],
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.green),
+                        )),
+                        Container(
+                          alignment: Alignment.topRight,
+                          padding: EdgeInsets.fromLTRB(100, 0, 10, 0),
+                          child: RaisedButton(
+                            textColor: Colors.white,
+                            color: Colors.black45,
+                            child: Text('View'),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20.0)),
+                            onPressed: () {
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (_) {
+                                return spareParts01();
+                              }));
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,124 +109,14 @@ class _sparePartsState extends State<spareParts> {
             fit: BoxFit.fill,
           ),
         ),
-        child: Column(
-          children: [
-            Column(
-              children: [
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10),
-                  padding: EdgeInsets.fromLTRB(10, 40, 10, 30),
-                  decoration: BoxDecoration(
-                      color: Colors.white60,
-                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                  child: Row(
-                    children: [
-                      Container(
-                          child: Image(
-                        image: AssetImage('lib/img/p1.PNG'),
-                      )),
-                      Column(
-                        children: [
-                          Container(
-                              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                              child: Text(
-                                '14" Alloy Wheels',
-                                style: TextStyle(
-                                    fontSize: 18, fontWeight: FontWeight.w500),
-                              )),
-                          Container(
-                              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                              child: Text(
-                                'LKR 30000',
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w500),
-                              )),
-                          Container(
-                              child: Text(
-                            'Available',
-                            style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.green),
-                          )),
-                          Container(
-                            alignment: Alignment.topRight,
-                            padding: EdgeInsets.fromLTRB(100, 0, 10, 0),
-                            child: RaisedButton(
-                              textColor: Colors.white,
-                              color: Colors.black45,
-                              child: Text('View'),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20.0)),
-                              onPressed: () {
-                                Navigator.push(context,
-                                    MaterialPageRoute(builder: (_) {
-                                  return spareParts01();
-                                }));
-                              },
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10),
-                  padding: EdgeInsets.fromLTRB(10, 40, 10, 30),
-                  decoration: BoxDecoration(
-                      color: Colors.white60,
-                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                  child: Row(
-                    children: [
-                      Container(
-                          child: Image(
-                        image: AssetImage('lib/img/p2.PNG'),
-                      )),
-                      Column(
-                        children: [
-                          Container(
-                              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                              child: Text(
-                                'Side Mirrors',
-                                style: TextStyle(
-                                    fontSize: 18, fontWeight: FontWeight.w500),
-                              )),
-                          Container(
-                              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                              child: Text(
-                                'LKR 50000',
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w500),
-                              )),
-                          Container(
-                              child: Text(
-                            'Available',
-                            style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.green),
-                          )),
-                          Container(
-                            alignment: Alignment.topRight,
-                            padding: EdgeInsets.fromLTRB(100, 0, 10, 0),
-                            child: RaisedButton(
-                              textColor: Colors.white,
-                              color: Colors.black45,
-                              child: Text('View'),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20.0)),
-                              onPressed: () {},
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ],
+        height: double.infinity,
+        child: FirebaseAnimatedList(
+          query: _ref,
+          itemBuilder: (BuildContext context, DataSnapshot snapshot,
+              Animation<double> animation, int index) {
+            Map contact = snapshot.value;
+            return _buildContactItem(contact: contact);
+          },
         ),
       ),
     );
